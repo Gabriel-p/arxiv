990f062bec3b27ec621d03bb2307bcd943a65841
Gabriel <gabrielperren@gmail.com>
2026-05-08 11:11:51 -0300

    fixing bug where empty file is written

--- scripts/fetch-arxiv.py
+++ scripts/fetch-arxiv.py
@@ -23,7 +23,7 @@ MINIMUM_SCORE = 0.0
 N_DAYS_BACK = 30
 # Points subtracted per day of article age
 SCORE_DECAY_PER_DAY = 0.25
-FILE_NAME = "arxiv.json"
+FILE_NAME = "arxiv_new.json"
 # Multiplier applied separately in score_keywords()
 title_weight = 3.0
 
